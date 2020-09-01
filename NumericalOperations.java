a01131d7084ed7ed8bd5460f0dde860cafa4eb20
JesseSal <23jesse.salinas@boiseschools.net>
2020-09-01 15:43:08 +0000

    Completed Assignment

---
+++ NumericalOperations.java
@@ -0,0 +1,13 @@
+public class NumericalOperations{
+  public static void main (String args[]){
+    String number ="1234";
+    String a = number.substring(0,1);
+    String b = number.substring(1,2);
+    String c = number.substring(2,3);
+    String d = number.substring(3,4);
+    System.out.print(d);
+    System.out.print(c);
+    System.out.print(b);
+    System.out.print(a);
+  }
+}
